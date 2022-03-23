47a600a1ecf7ebeea3ec3fd408339d93502c488e
yamikhailov <yaroslavmikhailov@tuta.io>
2022-03-23 20:46:54 +0200

    ui improvements

--- ang-app/src/app/app-routing.module.ts
+++ ang-app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './sections/home/home.component';
 import { PackagesComponent } from './sections/packages/packages.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'packages', component: PackagesComponent}
+  {path: 'packages', component: PackagesComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
--- ang-app/src/app/app.module.ts
+++ ang-app/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './sections/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PackagesComponent } from './sections/packages/packages.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    PackagesComponent
+    PackagesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
